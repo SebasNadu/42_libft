1305ffd2278316568e4646f7b65c1d662ca29bb1
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-05-03 11:09:16 +0200

    ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 10:46:58 by johnavar          #+#    #+#             */
+/*   Updated: 2023/05/03 11:09:01 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	s_len;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	s_len = 0;
+	while (s[s_len])
+		s_len++;
+	if (len > s_len - start)
+		len = s_len - start;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == 0)
+		return (NULL);
+	i = start;
+	j = 0;
+	while (s[i] && j < len)
+	{
+		str[j] = s[i];
+		i++;
+		j++;
+	}
+	str[j] = 0;
+	return (str);
+}
