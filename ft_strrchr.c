ec8d63f6108ebef1cfe3ccd49f0b1d14a161ae30
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-05-03 07:56:24 +0200

    ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 07:44:03 by johnavar          #+#    #+#             */
+/*   Updated: 2023/05/03 07:51:29 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (c == 0)
+		return ((char *)s + i);
+	while (i >= 0)
+	{
+		if (s[i] == c)
+			return ((char *)s + i);
+		i--;
+	}
+	return (NULL);
+}
