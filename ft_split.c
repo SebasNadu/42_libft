46d7fef9e19d53339c504ba7f531e5a9507ac1da
Johan Sebastian Navarro Duarte <johnavar@c4a3c8.42berlin.de>
2023-05-04 14:23:35 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,31 @@
 /*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 13:15:24 by johnavar          #+#    #+#             */
-/*   Updated: 2023/05/04 13:58:30 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/05/04 14:22:32 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	words_count(char const *s, char c)
+static int	ft_words_count(char const *s, char c)
 {
-		int	i;
+	int	i;
+	int	count;
 
-		i = 1;
-		while (s[i])
-			
+	i = 1;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == (const char)c && s[i + 1] != '\0')
+			count++;
+		i++;
+	}
+	return (count);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**result;
+
+	result = (char **) malloc(sizeof(char *) * ft_words_count(s, c));
 }
