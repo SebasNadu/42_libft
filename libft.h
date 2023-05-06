5fff45e8d39a001773abbec1dc956bb750c82a45
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-05-06 12:59:52 +0200

    lstdelone

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 23:02:00 by johnavar          #+#    #+#             */
-/*   Updated: 2023/05/06 12:12:10 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/05/06 12:59:30 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,11 @@ typedef struct s_list
 }					t_list;
 
 t_list	*ft_lstnew(void *content);
-
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
