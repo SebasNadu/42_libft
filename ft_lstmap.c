803b23ea81d3a2398e2a3a286a189b825392c18c
Johan Sebastian Navarro Duarte <johnavar@c4a3c8.42berlin.de>
2023-05-10 19:26:51 +0200

    done'

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 13:25:15 by johnavar          #+#    #+#             */
-/*   Updated: 2023/05/06 13:50:43 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/05/10 19:13:51 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new_lst = 0;
 	while (lst)
 	{
-		tmp = ft_lstnew(f(lst->content));
+		tmp = (t_list *)malloc(sizeof(t_list));
 		if (!tmp)
 		{
 			ft_lstclear(&new_lst, del);
 			return (0);
 		}
+		tmp->content = f(lst->content);
+		tmp->next = NULL;
 		ft_lstadd_back(&new_lst, tmp);
 		lst = lst->next;
 	}
