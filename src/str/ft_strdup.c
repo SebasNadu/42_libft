5b81cf24c765981a0244b8b166493103219d29a1
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-07-04 18:03:12 +0200

    reorder libft

---
+++ src/str/ft_strdup.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 10:29:52 by johnavar          #+#    #+#             */
+/*   Updated: 2023/07/04 17:29:49 by sebasnadu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = ft_strlen(s);
+	str = (char *)malloc(sizeof(*str) * (j + 1));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s, j + 1);
+	return (str);
+}
