ca724288ed1232f98c480c43afc7005f59ea82ce
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-11-23 19:56:19 +0100

    add conditional if NULL on strdup

--- src/str/ft_strdup.c
+++ src/str/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 10:29:52 by johnavar          #+#    #+#             */
-/*   Updated: 2023/07/04 19:02:29 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2023/11/23 19:55:31 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ char	*ft_strdup(const char *s)
 	int		i;
 	char	*str;
 
+	if (!s)
+		return (NULL);
 	i = ft_strlen(s);
-	str = (char *)malloc(sizeof(*str) * (i + 1));
+	str = (char *)malloc(sizeof(char) * (i + 1));
 	if (!str)
 		return (NULL);
 	ft_memcpy(str, s, i + 1);
