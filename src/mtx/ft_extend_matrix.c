07d5514f29fab20dacd9c0bea23f9d71391f5d9b
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-12-04 11:37:00 +0100

    add safetly ft_extend_matrix

--- src/mtx/ft_extend_matrix.c
+++ src/mtx/ft_extend_matrix.c
@@ -6,11 +6,25 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 12:30:30 by sebasnadu         #+#    #+#             */
-/*   Updated: 2023/11/21 12:30:42 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2023/12/04 11:36:00 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
+
+static void	ft_extend_loop(char ***matrix, char ***new, int len, int *i)
+{
+	while (++(*i) < len)
+	{
+		*new[*i] = *matrix[*i];
+		if (!*new[*i])
+		{
+			ft_matrixfree(matrix);
+			ft_matrixfree(new);
+			return ;
+		}
+	}
+}
 
 char	**ft_extend_matrix(char **matrix, char *str)
 {
@@ -27,16 +41,12 @@ char	**ft_extend_matrix(char **matrix, char *str)
 	if (!new)
 		return (NULL);
 	new[len + 1] = NULL;
-	while (++i < len)
-	{
-		new[i] = matrix[i];
-		if (!new[i])
-		{
-			ft_matrixfree(&matrix);
-			ft_matrixfree(&new);
-		}
-	}
+	ft_extend_loop(&matrix, &new, len, &i);
+	if (!matrix)
+		return (NULL);
 	new[i] = ft_strdup(str);
+	if (!new[i])
+		ft_matrixfree(&new);
 	ft_matrixfree(&matrix);
 	return (new);
 }
