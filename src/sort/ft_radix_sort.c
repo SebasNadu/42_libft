69659736761db41da521e7a51f24d6f33e9011b5
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-02-09 20:16:40 +0100

    Fix radix sort, Add simple implementation

--- src/sort/ft_radix_sort.c
+++ src/sort/ft_radix_sort.c
@@ -6,43 +6,33 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 23:28:52 by sebasnadu         #+#    #+#             */
-/*   Updated: 2024/02/09 17:21:28 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2024/02/09 20:11:41 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/libft.h"
 
-inline static long	ft_labs(long nbr)
+static int	get_max(int *nbrs, int len)
 {
-	if (nbr < 0)
-		return (-nbr);
-	return (nbr);
-}
+	int	i;
+	int	max;
 
-static int	get_max(long *nbrs, int len)
-{
-	long	max;
-	int		i;
-
-	max = ft_labs(nbrs[0]);
-	i = 1;
-	while (i < len)
-	{
-		if (ft_labs(nbrs[i]) > max)
-			max = ft_labs(nbrs[i]);
-		++i;
-	}
+	max = nbrs[0];
+	i = 0;
+	while (++i < len)
+		if (nbrs[i] > max)
+			max = nbrs[i];
 	return (max);
 }
 
-static void	counting_sort(int *nbrs, int len, int base, int exponent)
+static void	counting_sort(int nbrs[], int len, int base, int exponent)
 {
-	long	*output;
-	int		*counter;
-	int		i;
+	int	*output;
+	int	*counter;
+	int	i;
 
+	output = ft_calloc(len, sizeof(int));
 	counter = ft_calloc(base, sizeof(int));
-	output = ft_calloc(len, sizeof(long));
 	i = -1;
 	while (++i < len)
 		counter[(nbrs[i] / exponent) % base]++;
@@ -52,8 +42,8 @@ static void	counting_sort(int *nbrs, int len, int base, int exponent)
 	i = len;
 	while (--i >= 0)
 	{
-		output[counter[(ft_labs(nbrs[i]) / exponent) % base] - 1] = nbrs[i];
-		counter[(ft_labs(nbrs[i]) / exponent) % base]--;
+		output[counter[(nbrs[i] / exponent) % base] - 1] = nbrs[i];
+		counter[(nbrs[i] / exponent) % base]--;
 	}
 	i = -1;
 	while (++i < len)
@@ -66,18 +56,12 @@ static void	counting_sort(int *nbrs, int len, int base, int exponent)
 
 void	ft_radix_sort(int *nbrs, int len, int base)
 {
-	long	max;
-	long	exponent;
-	long	*lnbrs;
+	int	max;
+	int	exponent;
 
-	if (!nbrs || len < 2)
-		return ;
-	if (base < 2)
-		base = 10;
-	lnbrs = (long *)nbrs;
-	max = get_max(lnbrs, len);
+	max = get_max(nbrs, len);
 	exponent = 1;
-	while (max / exponent > 0)
+	while (max / exponent >= 1)
 	{
 		counting_sort(nbrs, len, base, exponent);
 		exponent *= base;
